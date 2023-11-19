fc528e52f3bb85ee0b17a3bed15578fee520c200
trumad <9892909+trumad@users.noreply.github.com>
2023-11-19 10:51:40 +0000

    Update kanbaknockout.js

--- js/kanbaknockout.js
+++ js/kanbaknockout.js
@@ -1,34 +1,14 @@
-const state = {
-    someProperty: ko.observable('Our property');
-};
 
-ko.store.setState(state);
 
-function ViewModel1(params) {
+function AppViewModel(params) {
     const self = this;
-    self.someProperty = params.someProperty; // from the app state, mapped by mapStateToParams
+    self.message = ko.observable('Just getting started...');
+    self.cats = params.cats;    // from the state object, see mapStateToParams below
+    self.selectCat = function(cat) {
+        params.selectedCat(cat);    // also from the state object
+    }
+
 }
-
-function mapStateToParams(state) { // 'state' here is the result of getState()()
-
-    // the properties of the returned object will be attached to params
-    // when the view model is instantiated
-    return {
-         someProperty: state.someProperty
-    };
-}
-
-const AppViewModel = connect(mapStateToParams)(ViewModel1);
-
-// function AppViewModel(params) {
-//     const self = this;
-//     self.message = ko.observable('Just getting started...');
-//     self.cats = params.cats;    // from the state object, see mapStateToParams below
-//     self.selectCat = function(cat) {
-//         params.selectedCat(cat);    // also from the state object
-//     }
-
-// }
 
 
 
@@ -46,7 +26,7 @@ async function fetchFromApi({ url, method = 'GET', postData = undefined }) {
       const response = await fetch(url, {
         method,
         headers: {
-          'Content-Type': 'application/json', // Adjust the content type as needed
+          'Content-Type': 'application/json', 
         },
         body: postData ? JSON.stringify(postData) : undefined,
       });
@@ -59,7 +39,7 @@ async function fetchFromApi({ url, method = 'GET', postData = undefined }) {
       return data;
     } catch (error) {
       console.error('Error fetching data:', error.message);
-      throw error; // Re-throw the error for the calling code to handle
+      throw error; 
     }
   }
 
